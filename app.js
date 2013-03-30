a00839224e3975499a2e54a94d7b7df4ce02df05
Sergey Rudenko <rudenko.sergey92@gmail.com>
2013-03-30 21:30:47 +0200

    Fixed

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var config = require('./config').config,
     events = require('./routes/events'),
     passportConfig = require("./passport-config"),
     passport = require('passport'),
-    cons = require('consolidate'),
     mongoose = require('mongoose');
 
 mongoose.connect(config.mongo.adress);
@@ -33,8 +32,8 @@ app.configure(function () {
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(passport.initialize());
     app.use(passport.session());
-    app.set('views', __dirname + '/views');
-    app.engine('html', require('ejs').renderFile);
+    app.engine('html', require('ejs').__express);
+    app.set('views', __dirname + '/public');
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
 });
